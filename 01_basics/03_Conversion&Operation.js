96ab2b7e5ed40b741ce3be5e1a46ce596cd9ba57
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-01-24 13:56:53 +0000

    ConversionOperation

---
+++ 01_basics/03_Conversion&Operation.js
@@ -0,0 +1,23 @@
+//let score = undefined
+
+//console.log(typeof score);
+
+//let valueinNumber = Number(score)
+//console.log(valueinNumber);
+
+//"33"=33
+//"33abc"=NaN
+//true =1,false=0
+
+let isLoggedIn = 1
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=True 0=False
+//""=False ,"sg"=True
+let number=99
+let StringNumber=String(number)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
