b251cff2f0575f5b6db656e22a7a26aa037fc3b8
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-01-24 13:35:52 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all JS code as newer version
+
+ 
+ // we are using node js  (alert(3+3)) Error not browser 
+console.log(3+3);
+
+//dataTypes
+ let name="Sharath"
+ let age =15
+ let isLoggedIn=true
+ let jo 
+ //number => 2 to power 53
+ //bigInt =>    used in stocks 
+ //string => ""
+ //boolean => true/false
+ //null => standaloneValue (representing free space )
+ //undefined => 
+// symbol => unique
+
+ //object
+console.log(typeof jo );
+
+
+
+
