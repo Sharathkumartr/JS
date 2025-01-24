53f9b4163efdc7a8b34962950fb2cd39bd00f07e
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-01-24 13:02:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "sharathkumartr@sc.com"
+var accountPassword = "165640"
+accountCity ="Bangalore"
+let accountState=
+
+//accountId = 2 // not allowed 
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail ="scsc@sc.com"
+accountPassword ="25256515"
+accountCity="Kerala"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ])
