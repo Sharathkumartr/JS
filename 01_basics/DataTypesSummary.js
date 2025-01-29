e26b1ba72baf442f14ba51b335a0ecb1d70482d5
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-01-29 04:15:19 +0000

    Datatypes,Strings

---
+++ 01_basics/DataTypesSummary.js
@@ -0,0 +1,30 @@
+// Primitive
+//7 Types - String -number -boolean -none -undefined -BigInt -Symbol
+
+
+const  score =2100
+const scoreValue =2100.78
+const isLoggedIn =false
+const outsidetemp=null
+let userrEmail;
+
+const id =Symbol('123')
+const Anotherid =Symbol('123')
+console.log(id==Anotherid);
+
+
+//JavaScript is DynamicallyTyped programming language because
+//Reference DataType/non-primitive 
+//3-arrays,objects,functions
+ const heros=["SuperMan","SpiderMan","IronMan"]
+
+let myOnj={
+    name:"Sharath",
+    Age:23,
+
+ }
+ const muFunction = function(){
+    console.log("Hello");
+    
+
+ }
