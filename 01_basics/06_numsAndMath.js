846f163a63e55e54c28b8dfebf24744c73f98472
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-02-01 04:40:45 +0000

    num and math

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());
+///console.log(balance.toFixed(1));
+
+// const otherNumber= 23.5689
+// //console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000
+// //console.log(hundreds.toLocaleString('en-IN'));
+
+// ////////////////////////////MAths/////////////////////
+
+// console.log(Math);
+
+// console.log(Math.PI);
+// console.log(Math.abs(-123));
+// console.log(Math.round(9.8));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.min(1,0,5,56));
+// console.log(Math.max(1,0,5,56));
+
+// console.log(Math.random());
+
+
+
+
+
+
+
+
+
