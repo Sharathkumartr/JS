b23fd1306fa02222b81ab3f697d45160bc4945bb
SHARATH KUMAR T R <125959089+Sharathkumartr@users.noreply.github.com>
2025-02-01 05:41:46 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,45 @@
+                            //array
+
+const myArr =[0 ,1,2,3,4,5,6,7,8,9]
+
+const  myArr2= new Array(1,5,6,7,8)
+
+// console.log(myArr2[4]);
+
+                          // Array Methods
+
+// myArr.push(9)// adds 9 to last index
+
+
+
+// myArr.pop(9)//remove last index array element
+// console.log(myArr);
+
+// myArr.unshift(10)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(0));
+
+// console.log(myArr.indexOf(9));
+
+// const newArr= myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+
+                   //slice 
+console.log("A",myArr);
+const myn1 =myArr.slice(1,3)
+console.log(myn1);
+                    //splice (used to add,replace)
+console.log("B",myArr);
+const myn2 =myArr.splice(1,3)
+console.log(myn2);
+
+console.log("C",myArr);
+console.log(myn2);
+
+
+
